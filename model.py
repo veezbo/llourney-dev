16e4267b1520497b56ec8a152827a452a0c74ca6
Vibhor Kumar <vibhor.kumar.me@gmail.com>
2023-08-12 01:08:11 -0700

    latest model, broken test

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch import Tensor
 import torch.nn as nn
+import torch.nn.functional as F
 
 from diffusers import AutoencoderKL
 from diffusers.models.embeddings import Timesteps
@@ -14,8 +15,10 @@ from transformers import AutoTokenizer, AutoModel
 
 from datasets import load_dataset
 
+from typing import Optional
+
 # Input image dimension
-IMG_DIM = 512
+IMG_DIM = 128
 
 # Patch dimension and latent quantities
 PATCH_DIM = 16
@@ -82,33 +85,56 @@ class LLourney(nn.Module):
         # LC*L*L/NP
         self.transformer_img_proj = nn.Linear(EMBEDDING_DIM, LATENT_CHANNELS*LATENT_DIM*LATENT_DIM//NUM_PATCHES)
 
-    def forward(self, img: Tensor, input_text: list[str], timestep: list[int]) -> Tensor:
-        # img (B, 3, I, I)  NOTE: May have to reshape in order to meet this Pytorch convention
-        # input_text [B]: str
-        # timestep [B]: [0, T)
-        B, _, input_img_dim, _ = img.shape
-        assert B == len(input_text), "Batch dim in img must match length of input texts"
-        assert B == len(timestep), "Batch dim in img must match length of timesteps"
+    @torch.no_grad()
+    def encode_image(self, img: Tensor) -> Tensor:
+        if self.train:
+            latent_image = self.vae.encode(img).latent_dist.sample()
+        else:
+            latent_image = self.vae.encode(img).latent_dist.mean
+        return latent_image
+    
+    @torch.no_grad()
+    def decode_image_latents(self, denoised_latent_image: Tensor, as_numpy: bool = True) -> np.ndarray:
+        denoised_latent_image = 1 / self.vae.config.scaling_factor * denoised_latent_image
+        image = self.vae.decode(denoised_latent_image, return_dict=False)[0]
+        image = (image / 2 + 0.5).clamp(0, 1)
+        
+        if as_numpy:
+            # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
+            image = image.cpu().permute(0, 2, 3, 1).float().numpy()
+        
+        return image
+
+    def forward(self, latent_image: Tensor, input_ids: Tensor, timestep: Tensor, text_pad_mask: Optional[Tensor] = None) -> Tensor:
+        # latent_image # (B, LC, L, L) - latent image from VAE
+        # input_ids (B, T): str
+        # timestep (B): [0, MAX_DIFFUSE_TIMESTEP)
+        # text_pad_mask (B, T)
+        B, _, latent_img_dim, _ = latent_image.shape
+        assert latent_img_dim == LATENT_DIM, "Latent image dim must match pre-defined LATENT_DIM"
+        assert B == input_ids.shape[0], "Batch dim in img must match length of input texts"
+        assert B == timestep.shape[0], "Batch dim in img must match length of timesteps"
         
         # NOTE: We may want to expose access to this elsewhere and/or return in output
-        latent_image = self.vae.encode(img).latent_dist.mean  # (B, LC, L, L)
-        print("latent_image:", latent_image.shape)
+        # latent_image = self.vae.encode(img).latent_dist.sample()  # (B, LC, L, L)
+        # print("latent_image:", latent_image.shape)
         # L = LATENT_DIM
         # LC = LATENT_CHANNELS
 
         # L/(Patch Dim)^2 = Num Patches = NP
         # C contains three channels worth of info blown up to hidden embedding size (C)
         patch_embs = self.vae_img_patch_embed(latent_image)  # (B, NP, C)
-        print("patch_embs:", patch_embs.shape)
+        # print("patch_embs:", patch_embs.shape)
 
         # Embedding to encode the diffusion timestep
-        timestep_emb = self.timestep_embed(torch.tensor(timestep, device=DEVICE)).unsqueeze(dim=1)  # (B, 1, C)
-        print("timestep_emb:", timestep_emb.shape)
+        timestep_emb = self.timestep_embed(timestep).unsqueeze(dim=1)  # (B, 1, C)
+        # print("timestep_emb:", timestep_emb.shape)
 
         # Embedding to encode the text to condition on
-        text_idx = torch.tensor(self.llama_tokenizer(input_text).input_ids, dtype=torch.long, device=DEVICE)  # (B, T): [0, VOCAB_SIZE)
+        # text_idx = torch.tensor(self.llama_tokenizer(input_text).input_ids, dtype=torch.long, device=DEVICE)  # (B, T): [0, VOCAB_SIZE)
+        text_idx = torch.tensor(input_ids, dtype=torch.long, device=DEVICE)  # (B, T): [0, VOCAB_SIZE)
         text_embs = self.llama_token_embs(text_idx)  # (B, T, C)
-        print("text_embs:", text_embs.shape)
+        # print("text_embs:", text_embs.shape)
 
         # Concatenate all embeddings together along sequence dimension in a unified embedding for Llama
         # Because Llama is purely causal, put the text and timestep embeddings before patches 
@@ -117,11 +143,12 @@ class LLourney(nn.Module):
         # TODO(1): May need some padding here to ensure dim 1 is max sequence length and valid input to llama
         # ^^ PENDING a test forward call
         transformer_emb = torch.cat([text_embs, timestep_emb, patch_embs], dim=1)  # (B, T+1+NP, C)
-        print("transformer_emb pre llama:", transformer_emb.shape)
+        # print("transformer_emb pre llama:", transformer_emb.shape)
 
         # Push unified embedding through Llama transformer blocks
-        transformer_emb = self.llama(inputs_embeds=transformer_emb).last_hidden_state  # (B, S, C)
-        print("transformer_emb post llama:", transformer_emb.shape)
+        total_pad_mask = F.pad(text_pad_mask, (0, NUM_PATCHES+1), value=1)  # (B, T+1+NP)
+        transformer_emb = self.llama(inputs_embeds=transformer_emb, attention_mask=total_pad_mask).last_hidden_state  # (B, S, C)
+        # print("transformer_emb post llama:", transformer_emb.shape)
 
         # TODO(3): Use einops to maybe do the reshaping and shuffling in a single step while being a lot more obvious
         # Blast out the patches. Also includes a linear layer
@@ -129,7 +156,7 @@ class LLourney(nn.Module):
         # NOTE: Recover the latent image channels from hidden dimension
         # Pluck out last NP tokens correspoding to patch embeddings
         projected_img_emb = self.transformer_img_proj(transformer_emb[:, -NUM_PATCHES:, :])  # (S[B, :-NP, C]) @ (C, LC*L*L/NP) -> (B, NP, LC*L*L/NP)
-        print("projected_img_emb after projection:", projected_img_emb.shape)
+        # print("projected_img_emb after projection:", projected_img_emb.shape)
         
         # Pure reshaping to original image shape, no learnable parameters
         # (B, NP, LC*L*L/NP) -> (B, sqrt[NP], sqrt[NP], L/sqrt[NP], L/sqrt[NP], LC) | B*LC*L^2 on both sides
@@ -137,7 +164,7 @@ class LLourney(nn.Module):
         projected_img_emb = projected_img_emb.reshape(
             shape=(B, LATENT_DIM // PATCH_DIM, LATENT_DIM // PATCH_DIM, PATCH_DIM, PATCH_DIM, LATENT_CHANNELS)
         )
-        print("projected_img_emb after reshape:", projected_img_emb.shape)
+        # print("projected_img_emb after reshape:", projected_img_emb.shape)
         # (B, H, W, P, Q, C) = (B, sqrt[NP], sqrt[NP], L/sqrt[NP], L/sqrt[NP], LC)
         # (B, LC, sqrt[NP], L/sqrt[NP], sqrt[NP], L/sqrt[NP])
         projected_img_emb = torch.einsum("bhwpqc->bchpwq", projected_img_emb)
@@ -175,9 +202,9 @@ class LLourney(nn.Module):
 # input("waiting here for confirmation, press enter")
 # exit()
 
-model = LLourney()
-model.to(DEVICE)
-print(f"Number of model parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+# model = LLourney()
+# model.to(DEVICE)
+# print(f"Number of model parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
 
 # Singular forward pass with fake images
@@ -189,7 +216,7 @@ def forward_pass_test():
     A gentle breeze rustled the leaves of the towering trees, adding a melodious charm to the scene. The garden was indeed a paradise, a spectacle of nature's magnificence, where every creature lived in har
     """
     denoised_latent_img = model(img, [input_str]*BATCH_SIZE, [1]*BATCH_SIZE)
-    print(denoised_latent_img.shape)
+    # print(denoised_latent_img.shape)
     predicted_img = model.decode_image_latents(denoised_latent_img)
 
     predicted_img_pil = numpy_to_pil(predicted_img)
@@ -198,7 +225,9 @@ def forward_pass_test():
         im.save(f'pred_img_{i+1}.png')
 
 
+# forward_pass_test()
+
 # Load data for training
-dataset = load_dataset("lambdalabs/pokemon-blip-captions")
+# dataset = load_dataset("lambdalabs/pokemon-blip-captions")
 
 # Training Loop TODO(1)
